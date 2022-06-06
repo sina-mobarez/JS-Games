50f341eaf3ae793837c00688ec7512d2c8566367
asm <mubarriizz@gmail.com>
2022-06-06 17:17:43 +0430

    [fix] async not working, use sync to fetch word by ajax

--- Guess-Words/main.js
+++ Guess-Words/main.js
@@ -7,74 +7,78 @@ const guessWord = document.querySelector('#diff');
 var play = false;
 var temple;
 var score = 0;
+var noun;
 btn.addEventListener('click', function () {
     if (!play) {
         guess.classList.toggle('hidden');
         let array = generateWord();
         let dif = array.definition;
-        let noun = array.word;
+        noun = array.word;
+        noun = noun.toLowerCase();
         btn.innerHTML = 'Guess';
+        result.innerHTML = '';
         showWord.innerHTML = shuffleword(noun);
         guessWord.innerHTML = dif;
+        play = true;
+        score = 0;
     } else {
+        var g = guess.value;
         score++;
         result.innerHTML = 'your guess is ' + score;
-        if (noun == guess.value) {
-            result.innerHTML = 'you win by ' + score + ' guesses';
-        } else {
-            guess.innerHTML = '';
-        }
+        checkwin(noun, g);
+
     }
 })
 
 function generateWord() {
     let url = 'https://random-words-api.vercel.app/word';
     var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, false);
+    xhr.send();
     var word;
 
-    xhr.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            let response = JSON.parse(this.response)
-            word = response[0];
+    if (xhr.status == 200) {
+        let response = JSON.parse(xhr.response)
+        word = response[0];
 
-        }
-        else {
-            let arrayWord = [{
-                'word': 'apple',
-                'definition': 'an fruit, a famous tech brand'
-            },
-            {
-                'word': 'samsung',
-                'definition': 'a korean tech brand'
-            },
-            {
-                'word': 'hello',
-                'definition': 'used as a greeting or to begin a phone conversation.'
-            },
-            {
-                'word': 'request',
-                'definition': 'A message sent over a network to a server.'
-            },
-            {
-                'word': 'server',
-                'definition': 'A computer dedicated to running such programs.'
-            },
-            {
-                'word': 'ajax',
-                'definition': 'is a technique for accessing web servers from a web page.'
-            },
-            {
-                'word': 'madrid',
-                'definition': 'a city in spain'
-            }];
+    }
+    else {
+        let arrayWord = [{
+            'word': 'apple',
+            'definition': 'an fruit, a famous tech brand'
+        },
+        {
+            'word': 'samsung',
+            'definition': 'a korean tech brand'
+        },
+        {
+            'word': 'hello',
+            'definition': 'used as a greeting or to begin a phone conversation.'
+        },
+        {
+            'word': 'request',
+            'definition': 'A message sent over a network to a server.'
+        },
+        {
+            'word': 'server',
+            'definition': 'A computer dedicated to running such programs.'
+        },
+        {
+            'word': 'ajax',
+            'definition': 'is a technique for accessing web servers from a web page.'
+        },
+        {
+            'word': 'madrid',
+            'definition': 'a city in spain'
+        }];
 
-            let index = Math.floor(Math.random() * arrayWord.length);
-            word = arrayWord[index];
-        }
+        let index = Math.floor(Math.random() * arrayWord.length);
+        word = arrayWord[index];
+
+
     }
 
-    xhr.open('GET', url, true);
-    xhr.send();
+
 
     return word;
 }
@@ -88,4 +92,16 @@ function shuffleword(name) {
         array.splice(index, 1);
     }
     return newArray.join('');
+}
+
+
+function checkwin(n, g) {
+    if (n === g) {
+        result.innerHTML = 'you win by ' + score + ' guesses --- <h5>' + noun + '</h5>';
+        btn.innerHTML = 'Play Again';
+        guess.classList.toggle('hidden');
+        play = false;
+    } else {
+        guess.value = '';
+    }
 }
